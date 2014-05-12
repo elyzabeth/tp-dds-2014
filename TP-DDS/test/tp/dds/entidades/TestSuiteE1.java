fa04b3ea8425279232269eb9b7cb227afc4d805b
elyzabeth <elizabeth.sosa@gmail.com>
2014-05-12 12:49:47 -0300

    Agrego Test Suite para Entrega 1.

---
+++ TP-DDS/test/tp/dds/entidades/TestSuiteE1.java
@@ -0,0 +1,16 @@
+package tp.dds.entidades;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   TPE1Prioridad1.class,
+   TPE1Prioridad2.class,
+   TPE1Prioridad3.class,
+   TPE1Prioridad4.class
+})
+
+public class TestSuiteE1 {
+
+}
